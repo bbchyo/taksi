8f45c1d4c6a98d9f2ff8c49333cd9a455cfc9bf8
halil <halil@halil-MacBook-Air.local>
2026-03-15 17:13:23 +0300

    Cleanup: Removed unnecessary Python environment and scripts

--- rm_bg.py
+++
@@ -1,9 +0,0 @@
-import rembg
-import sys
-import PIL.Image
-
-if len(sys.argv) != 3:
-    print("Usage: python rm_bg.py <input> <output>")
-    sys.exit(1)
-
-rembg.remove(PIL.Image.open(sys.argv[1])).save(sys.argv[2])
